2d4fe9c9b481903177ee697d5746bbe46a155695
MaggieJiang7 <341050599@gapps.yrdsb.ca>
2024-05-03 15:29:21 -0400

    idk

--- treadmill_horror.py
+++ treadmill_horror.py
@@ -21,27 +21,41 @@ controller1 = Controller(ControllerType.PRIMARY)
 # variable = Motor(port number, gear ratio (green=18-1, red=36-1, blue=6-1), reverse)
 
 leftMotor = Motor(Ports.PORT20, GearSetting.RATIO_18_1, False)
-rightMotor = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
+rightMotor = Motor(Ports.PORT19, GearSetting.RATIO_18_1, True)
 drivetrain = DriveTrain(leftMotor, rightMotor)
 
-clawMotor = Motor(Ports.PORT7, GearSetting.RATIO_18_1, True)
-armMotor = Motor(Ports.PORT16, GearSetting.RATIO_18_1, True)
+armMotor1 = Motor(Ports.PORT9, GearSetting.RATIO_18_1, True)
+armMotor2 = Motor(Ports.PORTS10, GearSetting.RATIO_18_1, True)
+clawMotor = Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
 
-inertialSensor = Inertial(Ports.PORT9)
+# inertialSensor = Inertial(Ports.PORT9)
 
 
 def preauton():
     armMotor.set_stopping(HOLD)
-    inertialSensor.calibrate()
+    # inertialSensor.calibrate()
 
-def autonomous():
+def drivercontrol():
     #TODO: make some code that worhesdfsdfs lmao
     leftMotor.spin(FORWARD, controller1.axis3.position()/1.1 + (controller1.axis1.position() / 4), PERCENT)
     rightMotor.spin(FORWARD, controller1.axis3.position()/1.1 - (controller1.axis1.position() / 4), PERCENT)
 
     if (controller1.buttonL1.pressing()):
-        clawMotor0.spin(REVERSE, 100, PERCENT)
+        armMotor1.spin(REVERSE, 100, PERCENT)
+        armMotor2.spin(REVERSE, 100, PERCENT)
     elif (controller1.buttonL2.pressing()):
+        armMotor1.spin(FORWARD, 100, PERCENT)
+        armMotor2.spin(FORWARD, 100, PERCENT)
+    else:
+        armMotor1.stop()
+        armMotor2.stop()
+
+    if (controller1.buttonR1.pressing()):
+        clawMotor.spin(REVERSE, 100, PERCENT)
+    elif(controller1.buttonR2.pressing()):
         clawMotor.spin(FORWARD, 100, PERCENT)
     else:
         clawMotor.stop()
+
+while True:
+    drivercontrol()
